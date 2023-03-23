38f44c9b4448fdea1f6ef371af7e4bbff8042745
yoosple <yoosple93@gmail.com>
2023-03-23 19:15:07 +0900

    2023-03-23

---
+++ WPF_DataGrid/WPF_DataGrid/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace WPF_DataGrid.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public string TelNo { get; set; }
+        public string TelNo2 { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ WPF_DataGrid/WPF_DataGrid/ViewModels/VMEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using WPF_DataGrid.Models;
+
+namespace WPF_DataGrid.ViewModels
+{
+    public class VMEmployee
+    {
+        public ObservableCollection<Employee> EmployeeLists { get; set; }
+
+        public VMEmployee()
+        {
+            EmployeeLists = new ObservableCollection<Employee>
+                //();
+            {
+                new Employee { Id = 1, Name = "John Doe", Department = "HR" },
+                new Employee { Id = 2, Name = "Jane Doe", Department = "IT" },
+                new Employee { Id = 3, Name = "Bob Smith", Department = "Sales" }
+            };
+        }
+    }
+}
